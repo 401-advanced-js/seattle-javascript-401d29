9c1e5158c94fe785477160d2a24c138ed4571a99
shiratap <hirataspencemama@gmail.com>
2019-04-30 22:36:44 -0700

    Lab32

--- curriculum/class-32/lab/starter-code/forms/src/components/player.js
+++ curriculum/class-32/lab/starter-code/forms/src/components/player.js
@@ -1,60 +1,64 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import Form from 'react-jsonschema-form';
-import schema from '../schema.json';
+import Form from "react-jsonschema-form";
 
-import * as actions from '../store/players-actions.js';
+import * as actions from "../store/players-actions.js";
+const superagent = require("superagent");
 
 const uiSchema = {
-  _id: { 'ui:widget': 'hidden' },
-  __v: { 'ui:widget': 'hidden' },
+  _id: { "ui:widget": "hidden" },
+  __v: { "ui:widget": "hidden" }
 };
 
 class Player extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state = { schema: schema };
+    this.state = { schema: {} };
   }
+  componentDidMount = () => {
+    superagent
+      .get("https://api-js401.herokuapp.com/api/v1/players/schema")
+      .then(results => {
+        let schema = results.body;
+        this.setState({ schema });
+      })
+      .catch(e => console.log(e));
+  };
 
-  handleSubmit = (form) => {
-    if ( parseInt(this.props.id) >= 0 ) { 
-      this.props.handlePut({id:this.props.id, record:form.formData});  
-    }
-    else {
+  handleSubmit = form => {
+    if (parseInt(this.props.id) >= 0) {
+      this.props.handlePut({ id: this.props.id, record: form.formData });
+    } else {
       this.props.handlePost(form.formData);
     }
-  }
+  };
 
   render() {
     return (
       <div>
         <h3>Edit Player {this.props.id}</h3>
         <Form
-            schema={this.state.schema}
-            uiSchema={uiSchema}
-            formData={this.props.players[this.props.id]}
-            onSubmit={this.handleSubmit}
-          />
-    
+          schema={this.state.schema}
+          uiSchema={uiSchema}
+          formData={this.props.players[this.props.id]}
+          onSubmit={this.handleSubmit}
+        />
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  players: state.players,
+  players: state.players
 });
 
 const mapDispatchToProps = (dispatch, getState) => ({
   handlePost: payload => dispatch(actions.post(payload)),
-  handlePut: payload => dispatch(actions.put(payload)),
-
+  handlePut: payload => dispatch(actions.put(payload))
 });
-
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  mapDispatchToProps
 )(Player);
