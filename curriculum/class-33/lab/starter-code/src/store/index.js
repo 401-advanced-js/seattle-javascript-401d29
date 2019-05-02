b2ffd513f7c8a93c27b8484721cc1dfe55e775ee
shiratap <hirataspencemama@gmail.com>
2019-05-01 23:23:28 -0700

    Lab33

--- curriculum/class-33/lab/starter-code/src/store/index.js
+++ curriculum/class-33/lab/starter-code/src/store/index.js
@@ -1,1 +1,10 @@
-// Store
+import { createStore, combineReducers, applyMiddleware } from "redux";
+
+import thunk from "redux-thunk";
+import APIreducer from "./people-reducers.js";
+
+let reducers = combineReducers({
+  api: APIreducer
+});
+
+export default () => createStore(reducers, applyMiddleware(thunk));
